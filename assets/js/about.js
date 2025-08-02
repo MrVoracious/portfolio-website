e35292f0cc0b3efc4ccdf86e66a80747cd3cbf51
Mr_Voracious <vanditkad@gmail.com>
2025-08-02 10:23:54 +0530

    grid aesthetic

--- assets/js/about.js
+++ assets/js/about.js
@@ -313,7 +313,16 @@ function resetGame() {
     gameStarted = false;
     // ball.style.display = "none";
     resetGithub(true)
-    playBar.textContent = "PLAY";
+    playBar.innerHTML = `
+                    <div class="card-content">
+                        PLAY
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 640 640"><!--!Font Awesome Free v7.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+                            <path
+                                d="M187.2 100.9C174.8 94.1 159.8 94.4 147.6 101.6C135.4 108.8 128 121.9 128 136L128 504C128 518.1 135.5 531.2 147.6 538.4C159.7 545.6 174.8 545.9 187.2 539.1L523.2 355.1C536 348.1 544 334.6 544 320C544 305.4 536 291.9 523.2 284.9L187.2 100.9z" />
+                        </svg>
+                    </div>
+    `;
     playBar.style.width = funContainer.getBoundingClientRect().width + 'px';
     setPaddlePosition(funContainer.getBoundingClientRect().left)
     gameInterval = null;
@@ -405,4 +414,16 @@ function checkCollisions() {
             highlightReset()
         }, 300);
     }
+}
+
+
+document.getElementById("cards").onmousemove = e => {
+  for(const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  };
 }
