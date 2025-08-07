bf2bd7d7214b9c89af1bd95af06a5be6d2432ef3
Mr_Voracious <vanditkad@gmail.com>
2025-08-08 02:23:26 +0530

    contact

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -4,38 +4,31 @@ function isMobile() {
     return /Mobi|Android/i.test(navigator.userAgent) || (window.innerWidth <= 800 && window.innerHeight <= 800);
 }
 
-const fullName = document.getElementById("input");
-const email = document.getElementById("email");
-const number = document.getElementById("number");
-const msg = document.getElementById("msg");
 const form = document.getElementById("form");
 
+emailjs.init("UERsGRXdFfYb0cwRU");
+
 function sendEmail() {
+    const fullName = document.getElementById("fullName").value;
+    const email = document.getElementById("email").value;
+    const number = document.getElementById("number").value;
+    const msg = document.getElementById("msg").value;
 
-    const bodyMsg = `<b><h4>Name</h3></b> ${fullName.value} <br> <b><h4>Email</h3></b> ${email.value} <br> <b><h4>Phone Number</h3></b> ${number.value} <br> <b><h4>Message</h4></b> ${msg.value}`;
+    const templateParams = {
+        fullName: fullName,
+        email: email,
+        number: number,
+        msg: msg
+    };
 
-    Email.send({
-        SecureToken: '406d7ee0-94a6-4de1-8aef-9219b718f68d',
-        To: 'vanditkad@gmail.com',
-        From: "vanditkad@gmail.com",
-        Subject: `${fullName.value} would like to contact you`,
-        Body: bodyMsg
-    }).then(
-    message => {
-        if (message === "OK") {
-            alert(`${fullName.value}, your message was sent successfully!`);
-            form.reset();
-        } else {
-            alert("Something went wrong: " + message);
-            console.error("Email sending failed:", message);
-        }
-    }
-).catch(
-    error => {
-        alert("Email sending failed. Check console.");
-        console.error("SMTP.js error:", error);
-    }
-);
+    emailjs.send("service_kjwr7b2", "template_iyk02on", templateParams)
+        .then(function (response) {
+            alert(`${fullName}, your message was sent successfully!`);
+            document.getElementById("form").reset();
+        }, function (error) {
+            alert("Something went wrong. Check console.");
+            console.error("EmailJS error:", error);
+        });
 }
 
 form.addEventListener("submit", (e) => {
